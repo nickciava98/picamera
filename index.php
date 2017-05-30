dae4449922b7297bef46bb63ac20d44593992312
nickciava98 <n.ciavarella@outlook.it>
2017-05-30 15:08:23 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 	
 	include("Net/SFTP.php");
 	
-	$main = array("	<form action = '' method = 'post'>
-						<table>
+	$main = array("	<table cellspacing = '5'>
+						<tr>
+							<td>
+								<h1>
+									<font face = 'Arial'>
+										Connection to Raspberry Pi
+									</font>
+								</h1>
+							</td>
+						</tr>
+					</table>
+					
+					<form action = '' method = 'post'>
+						<table cellspacing = '5'>
 							<tr>
 								<td>
 									<font face = 'Arial'>
-										Raspberry Pi IP:
+										<b>
+											Raspberry Pi IP:
+										</b>
 									</font>
 								</td>
 							
@@ -20,7 +34,9 @@
 							<tr>
 								<td>
 									<font face = 'Arial'>
-										Username:
+										<b>
+											Username:
+										</b>
 									</font>
 								</td>
 								
@@ -32,7 +48,9 @@
 							<tr>
 								<td>
 									<font face = 'Arial'>
-										Password:
+										<b>
+											Password:
+										</b>
 									</font>
 								</td>
 								
@@ -42,7 +60,7 @@
 							</tr>
 							
 							<tr>
-								<table>
+								<table cellspacing = '5'>
 									<tr>
 										<td>
 											<input style = 'width: 100px' type = 'submit' name = 'connect' value = 'CONNECT'>
@@ -55,11 +73,39 @@
 								</table>
 							</tr>
 						</table>
-					</form>",
+					</form>
 					
-			"	<center>							
+					<table cellspacing = '5'>
+						<tr>
+							<td>
+								<font face = 'Arial'>
+									Copyright &copy 2017 Niccol&ograve Ciavarella. All rights reserved.
+								</font>
+							</td>
+						</tr>
+						
+						<tr>							
+							<td align = 'center'>
+								<a title = 'GNU Affero General Public License v3' href = 'http://www.gnu.org/licenses/agpl-3.0.txt' target = '_blank'><img src = 'agplv3.png'></a>
+							</td>
+						</tr>
+					</table>",
+					
+			"	<center>
+					<table cellspacing = '5'>
+						<tr>
+							<td>
+								<h1>
+									<font face = 'Arial'>
+										Take Photo from Raspberry Pi
+									</font>
+								</h1>
+							</td>
+						</tr>
+					</table>
+												
 					<form action = '' method = 'post'>
-						<table>
+						<table cellspacing = '5'>
 							<tr>
 								<td>
 									<input style = 'width: 100px' type = 'submit' name = 'photo' value = 'TAKE PHOTO'>
@@ -82,7 +128,26 @@
 						<div id = 'image'>
 							<img width = 800 height = 480>
 						</div>
-					</fieldset>");
+					</fieldset>
+					
+					<br>
+					
+					<table cellspacing = '5'>
+						<tr>
+							<td>
+								<font face = 'Arial'>
+									Copyright &copy 2017 Niccol&ograve Ciavarella. All rights reserved.
+								</font>
+							</td>
+						</tr>
+						
+						<tr>							
+							<td align = 'center'>
+								<a title = 'GNU Affero General Public License v3' href = 'http://www.gnu.org/licenses/agpl-3.0.txt' target = '_blank'><img src = 'agplv3.png'></a>
+							</td>
+						</tr>
+					</table>
+				</center>");
 	
 	print("	<html>
 				<title>
@@ -170,7 +235,7 @@
 		if(!$ssh->login($_COOKIE["user"], $_COOKIE["pass"]))
 		{
 			print("	<script>
-						alert('Error! Could not retrieve photo from Raspberry!');
+						alert('Error! Could not retrieve photo from Raspberry Pi!');
 						
 						document.cookie = 'c=0; expires=Fri, 31 Dec 2100 23:59:59 UTC';
 						document.cookie = 'ip=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
@@ -188,7 +253,7 @@
 			$ssh->get("/home/pi/Desktop/test.jpg", "test.jpg");	
 				
 			print("	<script>
-						image.innerHTML = '<img src = ".'"test.jpg"'." width = 800 height = 480>';
+						image.innerHTML = '<a href = 'test.jpg' target = '_blank'><img src = ".'"test.jpg"'." width = 800 height = 480></a>';
 					</script>");	
 		}
 	}	
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php	
+	print("	<html>
+				<title>
+					Test IMG
+				</title>
+				
+				<body>
+					<a href = 'test.jpg' target = '_blank'>					
+						<img src = 'test.jpg'>
+					</a>
+				</body>");
